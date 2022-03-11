4c986eedb320c8f67e4a7321d4b1a1943e6b8e81
phongteaa <phongteaa@gmail.com>
2022-03-11 14:16:26 +0700

    first commit

---
+++ Student.js
@@ -0,0 +1,32 @@
+class Student {
+
+    constructor(id, age, name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    setId(id) {
+        this.id = id;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    setAge(age) {
+        this.age = age;
+    }
+}
